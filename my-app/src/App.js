a35996e9b90a156de36e3affd60ad2f982501a9f
Reid Erdwien <rderdwien@gmail.com>
2023-06-12 20:11:58 -0600

    first draft

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div style={{ textAlign: 'center', padding: '50px', background: 'pink', color: 'purple' }}>
+      <h1>Welcome to Reid & Mariko's Wedding Website!</h1>
+      <p style={{ fontSize: '24px', margin: '50px auto', maxWidth: '600px' }}>
+        We're so excited to celebrate our special day with all our friends and family.
+        This website will provide all the details about our wedding. So stay tuned!
+      </p>
+      <div style={{ background: 'white', padding: '20px', borderRadius: '10px', display: 'inline-block', color: 'black' }}>
+        <h2>The Happy Couple</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <strong>Groom:</strong> Reid
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <strong>Bride:</strong> Mariko
+        </p>
+      </div>
     </div>
   );
 }
